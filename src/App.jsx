5d6ec3e0444f9ba18b4b0e25d25187bba32bd3f5
mszahan <mszahan1@gmail.com>
2023-11-21 18:52:27 +0600

    tried the create-react-app way to make the index.html work
    didn't work though

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-import './App.css';
-import {createRoot} from 'react-dom/client';
-
 
 const App = () => {
     return (
         <div className="app">
-            <h1>React official docs</h1>
+            <h1>React hola official docs</h1>
         </div>
     )
 }
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(<App/>);
+export default App;
---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import './App.css';
+import {createRoot} from 'react-dom/client';
+
+import App from './App';
+
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App/>);
