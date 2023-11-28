4717f5f1eb345cc3c6e46ed085f36dde91974cff
mszahan <mszahan1@gmail.com>
2023-11-28 23:51:26 +0600

    added empty array to track the state

--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -1,17 +1,16 @@
 import { useState, useTransition } from "react"
 
 
-function Square() {
-    const [value, setValue] = useState(null)
-    function handleClick() {
-        setValue('X');
+function Square({value}) {
+    // const [value, setValue] = useState(null)
+    // function handleClick() {
+    //     setValue('X');
 
-    }
+    // }
 
     return (
         <button 
         className="square"
-        onClick={handleClick}
         >
             {value}
         </button>
@@ -19,24 +18,25 @@ function Square() {
 }
 
 export default function Board () {
+    const [squares, setSquares] = useState((Array(9).fill(null)));
     return (
         <>
         <div className="board-row">
-            <Square/>
-            <Square />
-            <Square/>
+            <Square value={squares[0]}/>
+            <Square value={squares[1]}/>
+            <Square value={squares[2]}/>
         </div>
 
         <div className="board-row">
-            <Square/>
-            <Square />
-            <Square/>
+            <Square value={squares[3]}/>
+            <Square value={squares[4]}/>
+            <Square value={squares[5]}/>
         </div>
 
         <div className="board-row">
-            <Square/>
-            <Square/>
-            <Square />
+            <Square value={squares[6]}/>
+            <Square value={squares[7]}/>
+            <Square value={squares[8]}/>
         </div>
         </>
     )
