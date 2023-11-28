500e398cc2a68e72af97c4c820198f81c65c0f44
mszahan <mszahan1@gmail.com>
2023-11-29 00:37:42 +0600

    implemented the state with array this time

--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -1,7 +1,7 @@
 import { useState, useTransition } from "react"
 
 
-function Square({value}) {
+function Square({value, onSquareClick}) {
     // const [value, setValue] = useState(null)
     // function handleClick() {
     //     setValue('X');
@@ -11,6 +11,7 @@ function Square({value}) {
     return (
         <button 
         className="square"
+        onClick={onSquareClick}
         >
             {value}
         </button>
@@ -19,24 +20,31 @@ function Square({value}) {
 
 export default function Board () {
     const [squares, setSquares] = useState((Array(9).fill(null)));
+
+    function handleClick (indx) {
+        const nextSquare = squares.slice();
+        nextSquare[indx] = 'X';
+        setSquares(nextSquare);
+    }
+
     return (
         <>
         <div className="board-row">
-            <Square value={squares[0]}/>
-            <Square value={squares[1]}/>
-            <Square value={squares[2]}/>
+            <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
+            <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+            <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
         </div>
 
         <div className="board-row">
-            <Square value={squares[3]}/>
-            <Square value={squares[4]}/>
-            <Square value={squares[5]}/>
+            <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+            <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+            <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
         </div>
 
         <div className="board-row">
-            <Square value={squares[6]}/>
-            <Square value={squares[7]}/>
-            <Square value={squares[8]}/>
+            <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+            <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+            <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
         </div>
         </>
     )
