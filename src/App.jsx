727e8917743568eef1ba1f32bd78aef64babef15
mszahan <mszahan1@gmail.com>
2023-11-30 00:27:00 +0600

    created the static version of the Filterable Product Table

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Game from "./components/TicTacToe";
+import SearchThink from "./components/SearchThink";
 
 const App = () => {
     return (
         <div className="app">
             <h1>Tic Tac Toe</h1>
             <Game/>
+            <hr />
+            <h1>Thinking in React</h1>
+            <SearchThink/>
         </div>
     )
 }
