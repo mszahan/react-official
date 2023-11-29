c34e75e0323bd5ee46ef1e8f4c510564147ec096
mszahan <mszahan1@gmail.com>
2023-11-29 22:35:07 +0600

    implemented the history in the tictactoe game

--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -75,12 +75,37 @@ function Board ({ xIsNext, squares, onPlay }) {
 export default function Game() {
     const [xIsNext, setXIsNext] = useState(true);
     const [history, setHistory] = useState([Array(9).fill(null)]);
-    const currentSquares = history[history.length -1];
+    const [currentMove, setCurrentMove] = useState(0);
+    const currentSquares = history[currentMove];
 
     function handlePlay (nextSquare) {
-        setHistory([...history, nextSquare]);
+        const nextHistory = [...history.slice(0, currentMove + 1), nextSquare];
+        setHistory(nextHistory);
+        setCurrentMove(nextHistory.length - 1);
         setXIsNext(!xIsNext);
     }
+
+    function jumpTo(nextMove) {
+        setCurrentMove(nextMove);
+        setXIsNext(nextMove % 2 === 0);
+    }
+
+    const moves = history.map((squares, move) => {
+        let description;
+        if (move > 0) {
+            description = `Go to move # ${move}`;
+        } else {
+            description = 'Go to start point';
+        }
+        
+        return (
+            <li key={move}>
+                <button onClick={() => jumpTo(move)}>
+                    {description}
+                </button>
+            </li>
+        )
+    })
 
     return (
         <div className="game">
@@ -89,7 +114,7 @@ export default function Game() {
             </div>
 
             <div className="game-info">
-                <ol>{/*TODO*/}</ol>
+                <ol>{moves}</ol>
             </div>
         </div>
     )
