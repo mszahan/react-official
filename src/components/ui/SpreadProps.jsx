cb6472f74404c1edc11469243e8b1d2f41081972
mszahan <mszahan1@gmail.com>
2023-12-01 15:15:23 +0600

    used spread operator to spread the props and reduced repeatative code

---
+++ src/components/ui/SpreadProps.jsx
@@ -0,0 +1,44 @@
+function Avatar ({person, size, color, height}) {
+    return (
+        <div>
+            <p>name: {person.name}</p>
+            <p>age: {person.age}</p>
+            <p>role: {person.role}</p>
+            <p>Size of the product: {size}</p>
+            <p>color of the product {color} </p>
+            <p> Height of the product {height} </p>
+
+        </div>
+    )
+}
+
+function Card (props) {
+    return (
+        <div>
+            <Avatar {...props}/>
+        </div>
+    )
+}
+
+function Main () {
+    const person = {
+        name:'Roman',
+        age:25,
+        role:'CEO'
+    }
+
+    return (
+        <div>
+            <Card 
+            person={person}
+            size='100'
+            color='red'
+            height='25'
+            />
+        </div>
+    )
+
+
+}
+
+export default Main;
--- src/components/ui/Ui.jsx
+++ src/components/ui/Ui.jsx
@@ -1,11 +1,13 @@
 import { Profile } from "./Gallery";
 import Gallery from "./Gallery";
+import Main from "./SpreadProps";
 
 function Ui () {
     return (
         <div>
             <Gallery/>
             <Profile/>
+            <Main/>
         </div>
     )
 }
