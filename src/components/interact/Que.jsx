9c55d4acd5fc0361265e7d7c49a912643be582d9
mszahan <mszahan1@gmail.com>
2023-12-10 21:36:13 +0600

    explored the state update and rplacing the value

--- src/components/interact/Que.jsx
+++ src/components/interact/Que.jsx
@@ -6,10 +6,13 @@ function StateCounter () {
         setNum(n=> n+1)
         setNum(n=> n+1)
         setNum(n=> n+1)
+
     }
 
+
+
     return (
-        <div className="statecoutner">
+        <div className="statecoutner" id="statecoutner">
             <h1>Updating the state three times within same render</h1>
             <h1> {num} </h1>
             <div>
@@ -19,20 +22,50 @@ function StateCounter () {
                 >
                     +3
                 </button>
+
+
             </div>
         </div>
     )
 }
 
 
+function MixCounter () {
+    const [num, setNum] = useState(0);
+    function handleClick () {
+        setNum(num + 5)
+        setNum(n=> n+1)
+        setNum(n=> n+1)
+        setNum(num + 2)
+        
+
+    }
+
+    return (
+        <div className="statecoutner">
+            <h1>Updating the state after replacing the value first</h1>
+            <h1> {num} </h1>
+            <div>
+                <button
+                onClick={handleClick}
+                className="btn"
+                >
+                    +5
+                </button>
+            </div>
+        </div>
+    )
+}
 
 
 export default function Que() {
     return (
     <div className="que">
         <StateCounter/>
+        <MixCounter/>
 
     </div>
 
     )
 }
+
