9087c98197ace3149298bb0ff8fbefe775beff05
mszahan <mszahan1@gmail.com>
2023-12-12 19:48:38 +0600

    replaced the array elements with map funciton

--- src/components/interact/ArrayObject.jsx
+++ src/components/interact/ArrayObject.jsx
@@ -113,12 +113,52 @@ function ShapeEditor(){
     )
 }
 
+let initialCounters = [0, 0, 0];
+
+function CounterList() {
+    const [counters, setCounters] = useState(initialCounters);
+
+    function handleIncrementClick(index) {
+        const nextCounters = counters.map((c, i) => {
+            if (i===index){
+                return c + 1;
+            } else {
+                return c;
+            }
+        })
+
+        setCounters(nextCounters);
+    }
+
+
+    return (
+        <div className="counter-list">
+            <ul>
+                {
+                    counters.map((counter, i) => (
+                        <li key={i}>
+                            {counter} {' '}
+                            <button onClick={() => {
+                                handleIncrementClick(i);
+                            }}>
+                                +1
+                            </button>
+
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
+    )
+}
+
 
 export default function ArrayObject(){
     return (
         <div className="arrayObject">
             <List/>
-            <ShapeEditor/>
+            {/* <ShapeEditor/> */}
+            <CounterList/>
         </div>
     )
 }
