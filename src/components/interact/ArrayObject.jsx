660f903d7d852e12ac27685525ab42cf88bed119
mszahan <mszahan1@gmail.com>
2023-12-12 18:52:23 +0600

    transformed the array in state

--- src/components/interact/ArrayObject.jsx
+++ src/components/interact/ArrayObject.jsx
@@ -24,6 +24,8 @@ function List(){
                             ...artists,
                             {id: nextId++, name:name}
                         ])
+
+                        setName('')
                     }}
                     >
                         Add
@@ -53,10 +55,70 @@ function List(){
 }
 
 
+let initialShapes = [
+    {id:0, type:'circle', x:50, y:100},
+    {id:1, type:'square', x:150, y: 100},
+    {id:2, type:'circle', x:250, y: 100}
+]
+
+
+function ShapeEditor(){
+    const [shapes, setShapes] = useState(initialShapes);
+
+    function handleClick() {
+        const nextShapes = shapes.map(shape => {
+            if (shape.type === 'square') {
+                //no change
+                return shape;
+            } else {
+                //reutrn a new circle 50px below
+                return {
+                    ...shape,
+                    y:shape.y + 50,
+                }
+            }
+        })
+
+        setShapes(nextShapes)
+        
+    }
+
+    return (
+        <div className="movingshape">
+            <button onClick={handleClick}>
+                move circle down!
+            </button>
+            {
+                shapes.map(shape => (
+                    <div
+                    key={shape.id}
+                    style={{
+                        backgroundColor: 'blue',
+                        position: 'relative',
+                        left: shape.x,
+                        top:shape.y,
+                        borderRadius:
+                        shape.type === 'circle'
+                        ? '50%' : '',
+                        width:20,
+                        height:20,
+                    }}
+                    
+                    />
+                    
+
+                ))
+            }
+        </div>
+    )
+}
 
 
 export default function ArrayObject(){
     return (
-        <List/>
+        <div className="arrayObject">
+            <List/>
+            <ShapeEditor/>
+        </div>
     )
 }
