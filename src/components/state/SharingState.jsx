3a7dc5d0f61ed59550e732c1520641b70977562d
mszahan <mszahan1@gmail.com>
2023-12-17 11:51:53 +0600

    lifted the state up to the closest parent

--- src/components/state/SharingState.jsx
+++ src/components/state/SharingState.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 
 
-function Panel({title, children}) {
-    const [isActive, setIsActive] = useState(false);
+function Panel({title, children, isActive, onShow}) {
+    
 
     return (
         <section className="panel">
@@ -10,7 +10,7 @@ function Panel({title, children}) {
             {isActive ? (
                 <p> {children} </p>
             ) : (
-                <button onClick={() => setIsActive(true)}>
+                <button onClick={onShow}>
                     show
                 </button>
             )
@@ -21,14 +21,26 @@ function Panel({title, children}) {
 
 
 export default function Accordion() {
+    const [activeIndex, setActiveIndex] = useState(0);
 
     return (
         <div className="accordion">
-            <Panel title={'About'}>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero adipisci nam sit illo repellendus perferendis.
+            <Panel
+            title={'About'}
+            isActive={activeIndex ===0 }
+            onShow={ () => setActiveIndex(0)}
+            
+            >
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Libero adipisci nam sit illo repellendus perferendis.
             </Panel>
     
-            <Panel title={'Etymoloy'}>
+            <Panel
+            title={'Etymoloy'}
+            isActive={activeIndex ===1 }
+            onShow={ () => setActiveIndex(1)}
+
+            >
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Odio quidem ex et, aspernatur deleniti provident officiis labore culpa doloribus facilis!
             </Panel>
